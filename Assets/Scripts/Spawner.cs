989dbc968fb6c40a5f680c1f1b293b2222eae10e
Joe Arch <josephrarch@gmail.com>
2023-11-18 18:17:33 -0500

    more spawner stuff

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Spawner : MonoBehaviour
 {
@@ -11,7 +13,10 @@ public class Spawner : MonoBehaviour
     public float hazzardRate;
     public float patternRate;
     public float patternCooldown;
-    private float currentCooldown;
+
+    private List<GameObject> onCooldown;
+    private List<float> cooldowns;
+
     private System.Random rand;
 
     public float leftbound;
@@ -25,12 +30,23 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        cooldowns[0] = cooldowns[0] - Time.deltaTime;
+        while (cooldowns.Count > 0 && cooldowns[0] <= 0)
+        {
+            patterns.Add(onCooldown[0]);
+            onCooldown.RemoveAt(0);
+            cooldowns.RemoveAt(0);
+        }
         if (rand.NextDouble() <= leafRate * Time.deltaTime) SpawnLeaf();
+        if (patterns.Count > 0 && rand.NextDouble() <= patternRate * Time.deltaTime) SpawnPattern();
     }
 
     void SpawnPattern()
     {
-
+        int index = rand.Next(patterns.Count);
+        GameObject pattern = Instantiate(patterns[index], transform);
+        onCooldown.Add(patterns[index]);
+        // incomplete
     }
 
     void SpawnLeaf() {
