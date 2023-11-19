532f11511570d99a7b30c0840a8e6173fbd7e5d6
William Convertino <williamconvertino@gmail.com>
2023-11-19 18:00:58 -0500

    a

--- Assets/SpawnerManager.cs
+++ Assets/SpawnerManager.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class SpawnerManager : MonoBehaviour
 {
-    Spawner
+    private Spawner[] spawners;
+    public List<GameObject> players;
+
+    public float heightDiff = 10.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawners = GetComponentsInChildren<Spawner>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform top = players[0].transform;
+
+        if (players[1].transform.position.y > top.position.y) top = players[1].transform;
         
+        foreach (Spawner spawner in spawners)
+        {
+            spawner.active = Mathf.Abs(spawner.transform.position.y - top.transform.position.y) < heightDiff;
+        }
     }
 }
